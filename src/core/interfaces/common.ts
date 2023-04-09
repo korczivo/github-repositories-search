ba793c3e06348f9b588a18dcb44585fcad990c8a
korczivo <lukaszwp55@gmail.com>
2023-04-09 14:14:08 +0200

    add: table sorting, save table pagination and sort in query params URL

---
+++ src/core/interfaces/common.ts
@@ -0,0 +1,2 @@
+export type SortOrder = 'asc' | 'desc' | null;
+export type SortBy = 'name' | 'owner' | 'stars' | 'created' | null;
--- src/core/interfaces/tableManager.ts
+++ src/core/interfaces/tableManager.ts
@@ -1,6 +1,10 @@
+import { SortBy, SortOrder } from 'core/interfaces/common';
+
 export interface TableManagerProps {
   initialSearchTerm: string;
   initialPageSize: number;
+  initialSortBy: SortBy | string;
+  initialSortOrder: SortOrder | string;
   initialCurrentPage: number;
   pageParams: URLSearchParams;
 }
@@ -8,8 +12,13 @@ export interface TableManagerProps {
 export interface TableManagerResult {
   searchTerm: string;
   setSearchTerm: (searchTerm: string) => void;
+  sortBy: SortBy | string | null;
+  setSortBy: (sortBy: SortBy) => void;
+  sortOrder: SortOrder | string | null;
+  setSortOrder: (sortOrder: SortOrder | null) => void;
   currentPage: number;
   pageSize: number;
+  toggleSortDirectionAndSetKey: (key: SortBy) => SortBy;
   handlePageChange: (page: number) => void;
   handlePerPageChange: (newPerPage: number) => void;
 }
